9851faf038559de99bceff764ffb525d3fb8a02d
Efrain Hernandez <haefrain@gmail.com>
2022-05-30 14:02:00 -0500

    initial commit

---
+++ src/views/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ListUser msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ListUser from '@/components/ListUser.vue'
+
+export default {
+  name: 'UserView',
+  components: {
+    ListUser
+  }
+}
+</script>
